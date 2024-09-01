1d6b1256e79297b38bb96c7a6aa803b4a0bc19c5
Jeet Chauhan <48512478+jeetchauhan123@users.noreply.github.com>
2024-09-01 15:34:14 +0530

    creating class lms

--- pythonLMS.py
+++ pythonLMS.py
@@ -1,3 +1,11 @@
 import datetime
 import os
-os.getcwd()
+# os.getcwd()
+
+class LMS:
+    """
+    This class is used to keep records of books library.
+    It has total four modules: 'Display Books', 'Lend Books', 'Add Books', 'Return Books'
+    'list_of_books' should be txt file. 'library_name' should be string.
+    """
+  
