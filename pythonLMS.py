ca13bddad7c66b77cb43488b00dc78c30fde62ff
Jeet Chauhan <48512478+jeetchauhan123@users.noreply.github.com>
2024-09-01 16:59:42 +0530

    Update pythonLMS.py

--- pythonLMS.py
+++ pythonLMS.py
@@ -2,10 +2,19 @@ import datetime
 import os
 # os.getcwd()
 
-class LMS:
+class Library:
     """
     This class is used to keep records of books library.
     It has total four modules: 'Display Books', 'Lend Books', 'Add Books', 'Return Books'
     'list_of_books' should be txt file. 'library_name' should be string.
     """
-  
+
+    def __init__(self, list_of_books, library_name):
+        self.list_of_books = "Books_List.txt"
+        self.library_name = library_name
+        self.books_dict = {}
+        id = 101
+        with open(self.list_of_books) as b:
+            content = b.readlines()
+        for line in content:
+            self.books_dict.update()
